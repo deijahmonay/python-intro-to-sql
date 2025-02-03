dd44f63f0eaa0e166bb87cb3539e2fa0224350fd
deijahmonay <deijah@shack15.com>
2025-02-03 11:39:46 -0800

    pass exercise 3-7

--- clues.sql
+++ clues.sql
@@ -23,23 +23,39 @@ WHERE countries.code = countrylanguages.countrycode
 AND countries.region = 'Southern Europe'
 AND countrylanguages.language = 'Italian';
 
-             name              | code |     region      | language 
--------------------------------+------+-----------------+----------
- Italy                         | ITA  | Southern Europe | Italian
- San Marino                    | SMR  | Southern Europe | Italian
- Holy See (Vatican City State) | VAT  | Southern Europe | Italian
-(3 rows)
+--              name              | code |     region      | language 
+-- -------------------------------+------+-----------------+----------
+--  Italy                         | ITA  | Southern Europe | Italian
+--  San Marino                    | SMR  | Southern Europe | Italian
+--  Holy See (Vatican City State) | VAT  | Southern Europe | Italian
+-- (3 rows)
 
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
+SELECT countries.code, countries.name, countries.region, countrylanguages.language
+FROM countries
+JOIN countrylanguages 
+ON countries.code = countrylanguages.countrycode
+JOIN cities
+on countries.code = cities.countrycode
+WHERE region = 'Southern Europe' AND language = 'Italian' AND countries.name = cities.name;
+--  code |    name    |     region      | language 
+-- ------+------------+-----------------+----------
+--  SMR  | San Marino | Southern Europe | Italian
+-- (1 row)
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+SELECT countries.code, countries.name, countries.region, cities.name FROM countries JOIN cities ON countries.code = cities.countrycode WHERE region = 'South America' AND cities.name LIKE '%San Mar%';
 -- Write SQL query here
+
+--  code |   name    |    region     |        name        
+-- ------+-----------+---------------+--------------------
+--  ARG  | Argentina | South America | General San Martín
+-- (1 row)
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -47,6 +63,13 @@ AND countrylanguages.language = 'Italian';
 -- follow right behind you!
 
 -- Write SQL query here
+SELECT district FROM cities WHERE name LIKE 'General San Mart%';
+
+--    district   
+-- --------------
+--  Buenos Aires
+-- (1 row)
+
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -61,3 +84,10 @@ AND countrylanguages.language = 'Italian';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+ SELECT * FROM cities WHERE population = 91084;
+
+--    id  |     name     | countrycode |  district  | population 
+-- ------+--------------+-------------+------------+------------
+--  4060 | Santa Monica | USA         | California |      91084
+-- (1 row)
